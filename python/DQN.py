b3fafe24599908c51bcde63a99b52fd9cb57bd8a
dusty-nv <dustinf@nvidia.com>
2018-03-20 17:39:23 -0400

    cleanup DQN.py

--- python/DQN.py
+++ python/DQN.py
@@ -86,27 +86,9 @@ print('[deepRL]  allow_random:   ' + str(allow_random))
 print('[deepRL]  debug_mode:     ' + str(debug_mode))
 
 
-
-
-######################################################################
+#
 # Replay Memory
-# -------------
 #
-# We'll be using experience replay memory for training our DQN. It stores
-# the transitions that the agent observes, allowing us to reuse this data
-# later. By sampling from it randomly, the transitions that build up a
-# batch are decorrelated. It has been shown that this greatly stabilizes
-# and improves the DQN training procedure.
-#
-# For this, we're going to need two classses:
-#
-# -  ``Transition`` - a named tuple representing a single transition in
-#    our environment
-# -  ``ReplayMemory`` - a cyclic buffer of bounded size that holds the
-#    transitions observed recently. It also implements a ``.sample()``
-#    method for selecting a random batch of transitions for training.
-#
-
 Transition = namedtuple('Transition',
                         ('state', 'action', 'next_state', 'reward'))
 
@@ -132,79 +114,9 @@ class ReplayMemory(object):
         return len(self.memory)
 
 
-######################################################################
-# Now, let's define our model. But first, let quickly recap what a DQN is.
 #
-# DQN algorithm
-# -------------
+# Deep Q-Network
 #
-# Our environment is deterministic, so all equations presented here are
-# also formulated deterministically for the sake of simplicity. In the
-# reinforcement learning literature, they would also contain expectations
-# over stochastic transitions in the environment.
-#
-# Our aim will be to train a policy that tries to maximize the discounted,
-# cumulative reward
-# :math:`R_{t_0} = \sum_{t=t_0}^{\infty} \gamma^{t - t_0} r_t`, where
-# :math:`R_{t_0}` is also known as the *return*. The discount,
-# :math:`\gamma`, should be a constant between :math:`0` and :math:`1`
-# that ensures the sum converges. It makes rewards from the uncertain far
-# future less important for our agent than the ones in the near future
-# that it can be fairly confident about.
-#
-# The main idea behind Q-learning is that if we had a function
-# :math:`Q^*: State \times Action \rightarrow \mathbb{R}`, that could tell
-# us what our return would be, if we were to take an action in a given
-# state, then we could easily construct a policy that maximizes our
-# rewards:
-#
-# .. math:: \pi^*(s) = \arg\!\max_a \ Q^*(s, a)
-#
-# However, we don't know everything about the world, so we don't have
-# access to :math:`Q^*`. But, since neural networks are universal function
-# approximators, we can simply create one and train it to resemble
-# :math:`Q^*`.
-#
-# For our training update rule, we'll use a fact that every :math:`Q`
-# function for some policy obeys the Bellman equation:
-#
-# .. math:: Q^{\pi}(s, a) = r + \gamma Q^{\pi}(s', \pi(s'))
-#
-# The difference between the two sides of the equality is known as the
-# temporal difference error, :math:`\delta`:
-#
-# .. math:: \delta = Q(s, a) - (r + \gamma \max_a Q(s', a))
-#
-# To minimise this error, we will use the `Huber
-# loss <https://en.wikipedia.org/wiki/Huber_loss>`__. The Huber loss acts
-# like the mean squared error when the error is small, but like the mean
-# absolute error when the error is large - this makes it more robust to
-# outliers when the estimates of :math:`Q` are very noisy. We calculate
-# this over a batch of transitions, :math:`B`, sampled from the replay
-# memory:
-#
-# .. math::
-#
-#    \mathcal{L} = \frac{1}{|B|}\sum_{(s, a, s', r) \ \in \ B} \mathcal{L}(\delta) 
-#
-# .. math::
-#
-#    \text{where} \quad \mathcal{L}(\delta) = \begin{cases}
-#      \frac{1}{2}{\delta^2}  & \text{for } |\delta| \le 1, \\
-#      |\delta| - \frac{1}{2} & \text{otherwise.}
-#    \end{cases}
-#
-# Q-network
-# ^^^^^^^^^
-#
-# Our model will be a convolutional neural network that takes in the
-# difference between the current and previous screen patches. It has two
-# outputs, representing :math:`Q(s, \mathrm{left})` and
-# :math:`Q(s, \mathrm{right})` (where :math:`s` is the input to the
-# network). In effect, the network is trying to predict the *quality* of
-# taking each action given the current input.
-#
-
 class DQN(nn.Module):
 
 	def __init__(self):
@@ -301,32 +213,10 @@ class DRQN(nn.Module):
 		cx[:, :] = 0
 		return hx.detach(), cx.detach()
 
-######################################################################
-# Training
-# --------
-#
-# Hyperparameters and utilities
-# ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-# This cell instantiates our model and its optimizer, and defines some
-# utilities:
-#
-# -  ``Variable`` - this is a simple wrapper around
-#    ``torch.autograd.Variable`` that will automatically send the data to
-#    the GPU every time we construct a Variable.
-# -  ``select_action`` - will select an action accordingly to an epsilon
-#    greedy policy. Simply put, we'll sometimes use our model for choosing
-#    the action, and sometimes we'll just sample one uniformly. The
-#    probability of choosing a random action will start at ``EPS_START``
-#    and will decay exponentially towards ``EPS_END``. ``EPS_DECAY``
-#    controls the rate of the decay.
-# -  ``plot_durations`` - a helper for plotting the durations of episodes,
-#    along with an average over the last 100 episodes (the measure used in
-#    the official evaluations). The plot will be underneath the cell
-#    containing the main training loop, and will update after every
-#    episode.
-#
 
-
+#
+# Create model instance
+#
 print('[deepRL]  creating DQN model instance')
 
 lstm_actor_hx = lstm_actor_cx = None
@@ -371,6 +261,10 @@ memory = ReplayMemory(replay_mem)
 
 steps_done = 0
 
+
+#
+# Select agent action
+#
 def select_action(state, allow_rand):
 	global steps_done
 	global lstm_actor_hx
@@ -394,28 +288,12 @@ def select_action(state, allow_rand):
 #		print('[deepRL]  DQN selected exploratory random action')
 		return LongTensor([[random.randrange(num_actions)]])
 
-episode_durations = []
-
 print('[deepRL]  DQN script done init')
 
 
-######################################################################
-# Training loop
-# ^^^^^^^^^^^^^
 #
-# Finally, the code for training our model.
+# Training routine
 #
-# Here, you can find an ``optimize_model`` function that performs a
-# single step of the optimization. It first samples a batch, concatenates
-# all the tensors into a single one, computes :math:`Q(s_t, a_t)` and
-# :math:`V(s_{t+1}) = \max_a Q(s_{t+1}, a)`, and combines them into our
-# loss. By defition we set :math:`V(s) = 0` if :math:`s` is a terminal
-# state.
-
-
-last_sync = 0
-
-
 def optimize_model():
 	global last_sync
 	global lstm_batch_hx
@@ -487,23 +365,16 @@ def optimize_model():
 	optimizer.step()
 
 
-######################################################################
 #
-# Below, you can find the main training loop. At the beginning we reset
-# the environment and initialize the ``state`` variable. Then, we sample
-# an action, execute it, observe the next screen and the reward (always
-# 1), and optimize our model once. When the episode ends (our model
-# fails), we restart the loop.
+# C/C++ API hooks
 #
-# Below, `num_episodes` is set small. You should download
-# the notebook and run lot more epsiodes.
-
 last_action = None
 last_state = None
 curr_state = None
 last_diff = None
 curr_diff = None
 
+# callback for infering next action based on new state
 def next_action(state_in):
 	global last_state
 	global curr_state
@@ -543,6 +414,7 @@ def next_action(state_in):
 		return -1
 
 
+# callback for recieving reward and peforming training
 def next_reward(reward, end_episode):
 	global last_state
 	global curr_state
