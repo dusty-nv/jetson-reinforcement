94bbf208dcad84c9a7c3452d022dcedde33d79c6
dusty-nv <dustinf@nvidia.com>
2018-03-22 20:09:29 -0400

    launch gzserver/gzclient to improve rendering reliability

--- gazebo/gazebo-arm.sh
+++ gazebo/gazebo-arm.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 echo " "
-echo "configuring gazebo7 plugin paths"
+echo "configuring Gazebo7 plugin paths"
 echo "previous GAZEBO_PLUGIN_PATH=$GAZEBO_PLUGIN_PATH"
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 echo "script directory $SCRIPT_DIR"
@@ -10,8 +10,21 @@ MY_PLUGIN_PATH=$SCRIPT_DIR/../lib
 echo "plugin path $MY_PLUGIN_PATH"
 
 export GAZEBO_PLUGIN_PATH=$MY_PLUGIN_PATH:$GAZEBO_PLUGIN_PATH
-echo "new GAZEBO_PLUGIN_PATH=$GAZEBO_PLUGIN_PATH"
+echo "GAZEBO_PLUGIN_PATH=$GAZEBO_PLUGIN_PATH"
 
 echo " "
-echo "starting gazebo7 simulator"
-gazebo gazebo-arm.world --verbose
+
+echo "starting Gazebo7 Client (gzclient)"
+gnome-terminal -e 'sh -c "echo \"\033]0; Gazebo7 Client (gzclient)\007\"; \
+				echo \"launching Gazebo7 Client (gzclient)\"; \
+				echo \"Press Ctrl+Q or close window to quit\n\"; \
+				sleep 2; \
+				gzclient --verbose; \
+				pkill gzserver"' # pkill -INT gzserver
+
+echo "starting Gazebo7 Server (gzserver)\n"
+gzserver gazebo-arm.world --verbose
+
+echo "Gazebo7 Server (gzserver) has exited."
+
+#gazebo gazebo-arm.world --verbose
--- gazebo/gazebo-rover.sh
+++ gazebo/gazebo-rover.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 echo " "
-echo "configuring gazebo7 plugin paths"
+echo "configuring Gazebo7 plugin paths"
 echo "previous GAZEBO_PLUGIN_PATH=$GAZEBO_PLUGIN_PATH"
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 echo "script directory $SCRIPT_DIR"
@@ -16,5 +16,18 @@ export GAZEBO_MODEL_PATH=$SCRIPT_DIR:$GAZEBO_MODEL_PATH
 echo "GAZEBO_MODEL_PATH=$GAZEBO_MODEL_PATH"
 
 echo " "
-echo "starting gazebo7 simulator"
-gazebo gazebo-rover.world --verbose
+
+echo "starting Gazebo7 Client (gzclient)"
+gnome-terminal -e 'sh -c "echo \"\033]0; Gazebo7 Client (gzclient)\007\"; \
+				echo \"launching Gazebo7 Client (gzclient)\"; \
+				echo \"Press Ctrl+Q or close window to quit\n\"; \
+				sleep 2; \
+				gzclient --verbose; \
+				pkill gzserver"' # pkill -INT gzserver
+
+echo "starting Gazebo7 Server (gzserver)\n"
+gzserver gazebo-rover.world --verbose
+
+echo "Gazebo7 Server (gzserver) has exited."
+
+#gazebo gazebo-rover.world --verbose
